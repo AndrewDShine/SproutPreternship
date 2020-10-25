f68dca80ea7f7ace204b0750305af79221064cf0
AndrewDShine <andrew.d.shine@gmail.com>
2020-10-25 14:09:39 -0400

    Began universal class

---
+++ Universal.cpp
@@ -0,0 +1,18 @@
+/*
+ * Universal.cpp
+ *
+ *  Created on: Oct 25, 2020
+ *      Author: andre
+ */
+
+#include "Universal.h"
+
+Universal::Universal() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Universal::~Universal() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Universal.h
@@ -0,0 +1,17 @@
+/*
+ * Universal.h
+ *
+ *  Created on: Oct 25, 2020
+ *      Author: andre
+ */
+
+#ifndef UNIVERSAL_H_
+#define UNIVERSAL_H_
+
+class Universal {
+public:
+	Universal();
+	virtual ~Universal();
+};
+
+#endif /* UNIVERSAL_H_ */
